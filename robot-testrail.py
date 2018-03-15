ae2a08bfaf23da6a434c45ede8b7b79c845b19ca
Raphael Bickel <raphael@7peakssoftware.com>
2018-03-15 16:55:59 +0700

    Fixes

--- robot-testrail.py
+++ robot-testrail.py
@@ -20,17 +20,20 @@ def parse_uat_result(filename):
 				cidstatus[cid] = '1' if status[-1].attributes['status'].value=="PASS" else '5'
 	return cidstatus
 
+
 def main():
 
 	ssl._create_default_https_context = ssl._create_unverified_context
-	resultxml = "robot_result.xml"
+	resultxml = "output.xml"
 	projectid = 0
 	user = "admin"
 	pwd = ""
-	folder = ""
+	folder = "./"
+	suite_id = 33
+	run_name = "Automated test run"
 	testrail_url = "https://172.18.51.100/testrail/"
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "",["pid=", "folder=", "user=", "pwd=", "testrail="])
+		opts, args = getopt.getopt(sys.argv[1:], "",["pid=", "suiteid=", "folder=", "user=", "pwd=", "testrail=", "run="])
 	except getopt.GetoptError as err:
 		# print help information and exit:
 		print str(err) # will print something like "option -a not recognized"
@@ -40,27 +43,34 @@ def main():
 			folder = a;
 		elif o == "--pid":
 			projectid = a;
+		elif o == "--suiteid":
+			suite_id = a;
 		elif o == "--user":
 			user = a;
 		elif o == "--pwd":
 			pwd = a;
 		elif o == "--testrail":
 			testrail_url = a;
+		elif o == "--run":
+			run_name = a;			
 		else:
 			assert False, "Unknown option" + o
 		
-	print "User:" + user + ":" + pwd
-	print "Project ID:" + str(projectid) + " TestRail URL:" + testrail_url
+	print "User:" + user
+	print "Project ID:" + str(projectid)
+	print "TestRail URL:" + testrail_url
+	print "Suite ID:" + str(suite_id) 
+	print "Run:" + str(run_name) 
 	client = APIClient(testrail_url)
 	client.user = user
 	client.password = pwd
 
 	run = client.send_post('add_run/' + str(projectid), 
-		{"suite_id": 1,"name": "This is a new test run","assignedto_id": 1,"include_all": 	"1"})
+		{"suite_id": suite_id,"name": run_name,"assignedto_id": 1,"include_all":"1"})
 
 	for root, dirs, files in os.walk(folder):
 		for file in files:
-			if not fnmatch.fnmatch(file, '*_output.xml'):
+			if not fnmatch.fnmatch(file, '*output.xml'):
 				continue
 			cidstatus = parse_uat_result(file)		
 			for cid in iter(cidstatus):
@@ -70,4 +80,3 @@ def main():
 	print "Done!"
 
 main();
-
